8855c62d67d6944694152c8385384366f2a02343
MaddyLobb <81719007+MaddyLobb@users.noreply.github.com>
2022-03-06 11:02:09 -0500

    css/js

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,33 @@
-import { gsap } from "gsap";
+//import { gsap } from "gsap";
+
+//Darkmode//
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e){
+
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme',dark);
+    }
+
+    else{
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme',light); 
+    }
+    
+    console.log(toggleSwitch.checked +'this is the value for the checkbox');
+
+
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') :null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+    }
+}
